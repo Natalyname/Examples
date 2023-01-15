288125a6e41a1cb1fc0225d40f5f94a50922c200
Nataly <lalliana@list.ru>
2023-01-15 19:42:44 +0300

    Задача на перевод из Цельсия в Фаренгейт

---
+++ Example1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿// // Напишите функцию, которая принимает на вход градусы по Цельсию и возвращает градусы по фаренгейту.
+// Результат должен быть округлен до 1 знакапосле точки.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите градусы по Цельсию: ");
+float degrees = float.Parse(ReadLine()!);
+
+float farengeit = GetFarengeit(degrees);
+WriteLine($"{degrees} по Цельсию равно {farengeit} по фаренгейту.");
+
+
+float GetFarengeit(float X)
+{
+  float res = (X * 9/5) + 32; 
+   return res;
+}
