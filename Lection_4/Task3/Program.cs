3156e03baed0975a6c12f2c3b049c6dd6a4f0a17
Nataly <lalliana@list.ru>
2023-01-22 22:32:08 +0300

    Вычисление факториала. Рекурсия

---
+++ Lection_4/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вычисление факториала. Рекурсия.
+
+double Factorial(int n)
+{
+ // 1! = 1
+ // 0! = 1
+ if(n == 1) return 1;
+ else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+ Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+// E + 29 означает, что получившееся число нужно умножить на 10 в 29 степени.
