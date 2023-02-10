11857353b1c223e8fa04cd2a1bb720a5c5e8f132
Nataly <lalliana@list.ru>
2023-02-10 23:24:02 +0300

    switch case гласные и согласные буквы

---
+++ MyTasks/Task3/Program.cs
@@ -0,0 +1,44 @@
+﻿//Напишите программу, в которой пользователь вводит с клавиатуры букву.
+// Программа должна вывести " буква гласная" если введённая буква гласная и соответственно с согл. буквой.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите русскую букву: ");
+string letter = ReadLine()!;
+        switch (letter)
+        {
+            case "а":
+        WriteLine("буква гласная");
+        break;
+          case "у":
+        WriteLine("буква гласная");
+        break;
+          case "о":
+        WriteLine("буква гласная");
+        break;
+          case "ы":
+        WriteLine("буква гласная");
+        break;
+           case "и":
+        WriteLine("буква гласная");
+        break;   
+          case "э":
+        WriteLine("буква гласная");
+        break;
+          case "я":
+        WriteLine("буква гласная");
+        break;
+        case "ю":
+        WriteLine("буква гласная");
+        break;
+        case "е":
+        WriteLine("буква гласная");
+        break;
+        case "ё":
+        WriteLine("буква гласная");
+        break;
+        default:
+        WriteLine("буква согласная");
+        break;
+        }
